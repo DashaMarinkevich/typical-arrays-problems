482e20222af43709680240a539f6bf11a6b5bf00
DashaMarinkevich <dasha_marinkevich@mail.ru>
2021-02-26 15:14:46 +0300

    feat: add change index.js
    add change index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min = 0;
+    if (typeof array == 'undefined' || array.length == 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (typeof array == 'undefined' || array.length == 0) {
+        return 0;
+    } else {
+        let maxValue = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array == 'undefined' || array.length == 0) {
+        return 0;
+    } else {
+        let kol = array.length;
+        let sumValue = 0;
+        for (let i = 0; i < array.length; i++) {
+            sumValue += array[i];
+        }
+        let avgValue = sumValue / kol;
+        return avgValue;
+    }
 }
